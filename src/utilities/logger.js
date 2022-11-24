7be5630241ea0292787d8fb3d66f665a781b9f1d
Venkat Rimmalapudi <venkatrajrimmalapudi@gmail.com>
2022-11-24 13:06:02 +0530

    ✨ feat
    created custom winston logger

---
+++ src/utilities/logger.js
@@ -0,0 +1,29 @@
+const { createLogger, format, transports } = require('winston');
+
+function Logger (label) {
+
+    const logger = createLogger ({
+        format: format.combine (
+            format.label ({
+                label
+            }),
+            format.timestamp ({format: 'DD-MM-YYYY HH:mm:ss'}),
+            format.printf (({level, message, label, timestamp}) => {
+                return `${timestamp} ${level} [${label}] ${message}`;
+            }),
+        ),
+        transports: [
+            new transports.Console(),
+            new transports.File({ filename: 'logs/combined.log' })
+        ]
+    });
+
+    const getMessage = (label, message) => `[${label}]: ${message}`
+
+    this.info = (label, message) => logger.info (getMessage (label, message));
+    this.error = (label, message) => logger.error (getMessage (label, message));
+    this.debug = (label, message) => logger.debug (getMessage (label, message));
+    this.warn = (label, message) => logger.warn (getMessage (label, message));
+}
+
+module.exports.Logger = Logger;
