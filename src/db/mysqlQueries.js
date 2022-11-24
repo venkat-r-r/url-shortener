d8d3b2f911c4f736bfd38b3b28885a556da3d073
Venkat Rimmalapudi <venkatrajrimmalapudi@gmail.com>
2022-11-24 10:54:05 +0530

    ✨ feat
    added new helper methods and implemented existing function of urlShortener handler

--- src/db/mysqlQueries.js
+++ src/db/mysqlQueries.js
@@ -4,14 +4,21 @@ function MySqlQueries () {
 
     const isAliasExists = async (alias) => 
         (await executeQuery (`SELECT count(*) from urls WHERE alias = '${alias}'`))[0]['count(*)'] > 0;
+        
+    const getAlias = async (url) => 
+            (await executeQuery (`SELECT alias from urls WHERE url = '${url}'`));
+
+    const isUrlExists = async (url) => 
+        (await executeQuery (`SELECT count(*) from urls WHERE url = '${url}'`))[0]['count(*)'] > 0;
 
     const getUrl = async (alias) => 
         (await executeQuery (`SELECT url from urls WHERE alias = '${alias}'`));
 
+
     const saveUrl = async (alias, url) => 
         (await executeQuery (`INSERT INTO urls (alias, url) values ('${alias}', '${url}')`));
 
-    return {getUrl, saveUrl, isAliasExists};
+    return {getAlias, getUrl, saveUrl, isAliasExists, isUrlExists};
 }
 
 
--- src/services/urlShortener.js
+++ src/services/urlShortener.js
@@ -3,10 +3,39 @@ const mysqlQueries = require ('../db/mysqlQueries');
 
 function urlShortener () {
 
-    const getUrl = (alias) => {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+    const isUrlValid = (url) => !!pattern.test (url);
+
+    const generateAlias = async () => {
+        let alias;
+        do {
+            alias = Math.random().toString(36).slice (3);
+        } while (await mysqlQueries.isAliasExists (alias));
+        return alias;
     };
 
-    const saveUrl = (alias, url) => {};
+    const getUrl = async (alias) => (await mysqlQueries.getUrl (alias))[0]['url'];
+
+    const saveUrl = async (alias, url) => {
+        if (isUrlValid (alias)) {
+            if (alias === undefined) {
+                if (await mysqlQueries.isUrlExists (url)) {
+                    alias = (await mysqlQueries.getAlias (url))[0]['alias'];     
+                }
+                else {
+                    alias = await generateAlias ();
+                    await mysqlQueries.saveUrl (alias, url);
+                }
+                return alias;
+            }
+        }
+    };
 
     return {getUrl, saveUrl};
 }
